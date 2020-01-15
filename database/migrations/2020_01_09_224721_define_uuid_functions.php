ad07f9d549fbab14b32744d9f7be12ff3d8cb568
Justin Scott <justin@hitpath.com>
2020-01-15 16:10:34 -0600

    Fixes README and mysql functions.

--- database/migrations/2020_01_09_224721_define_uuid_functions.php
+++ database/migrations/2020_01_09_224721_define_uuid_functions.php
@@ -23,8 +23,8 @@ class DefineUuidFunctions extends Migration
         $mysqlVersionCheck = DB::select(DB::raw('SHOW VARIABLES LIKE "version";'));
         $mysqlVersion = explode('.',$mysqlVersionCheck[0]->Value);
 
+        $this->down();
         if((int)$mysqlVersion[0] < 8) {
-            $this->down();
             DB::unprepared(<<<SQL
                 -- Turns a uuid string into a binary representation
                 -- Behaves the same as UUID_TO_BIN( ,true)
@@ -55,6 +55,23 @@ class DefineUuidFunctions extends Migration
                 ));
             SQL);
         }
+    } else {
+        DB::unprepared(<<<SQL
+            -- Turns a uuid string into a binary representation
+            -- Behaves the same as UUID_TO_BIN( ,true)
+            CREATE FUNCTION uuid2bin(_uuid BINARY(36))
+            RETURNS BINARY(16)
+            LANGUAGE SQL DETERMINISTIC CONTAINS SQL SQL SECURITY INVOKER
+            RETURN UUID_TO_BIN(_uuid, 1);
+        SQL);
+
+        DB::unprepared(<<<SQL
+            -- Turns a 16 byte binary into a uuid string
+            CREATE FUNCTION bin2uuid(_bin BINARY(16))
+            RETURNS BINARY(36)
+            LANGUAGE SQL DETERMINISTIC CONTAINS SQL SQL SECURITY INVOKER
+            RETURN BIN_TO_UUID(_bin, 1);
+        SQL);
     }
 
     /**
@@ -64,7 +81,7 @@ class DefineUuidFunctions extends Migration
      */
     public function down()
     {
-        DB::unprepared("DROP FUNCTION IF EXISTS UUID_TO_BIN");
-        DB::unprepared("DROP FUNCTION IF EXISTS BIN_TO_UUID");
+        DB::unprepared("DROP FUNCTION IF EXISTS uuid2bin");
+        DB::unprepared("DROP FUNCTION IF EXISTS bin2uuid");
     }
 }
