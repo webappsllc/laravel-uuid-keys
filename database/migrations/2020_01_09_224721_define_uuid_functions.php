7c95546102a0bc4e301bd7e17bb234ccb1cf5e7d
Justin Scott <justin@hitpath.com>
2020-04-01 17:19:44 -0500

    Defines uuid and bin conversion funcs.

--- database/migrations/2020_01_09_224721_define_uuid_functions.php
+++ database/migrations/2020_01_09_224721_define_uuid_functions.php
@@ -28,7 +28,7 @@ class DefineUuidFunctions extends Migration
             DB::unprepared(<<<SQL
                 -- Turns a uuid string into a binary representation
                 -- Behaves the same as UUID_TO_BIN( ,true)
-                CREATE FUNCTION uuid2bin(_uuid BINARY(36))
+                CREATE FUNCTION reorder_uuid2bin(_uuid BINARY(36))
                 RETURNS BINARY(16)
                 LANGUAGE SQL DETERMINISTIC CONTAINS SQL SQL SECURITY INVOKER
                 RETURN
@@ -42,7 +42,7 @@ class DefineUuidFunctions extends Migration
 
             DB::unprepared(<<<SQL
                 -- Turns a 16 byte binary into a uuid string
-                CREATE FUNCTION bin2uuid(_bin BINARY(16))
+                CREATE FUNCTION reorder_bin2uuid(_bin BINARY(16))
                 RETURNS BINARY(36)
                 LANGUAGE SQL DETERMINISTIC CONTAINS SQL SQL SECURITY INVOKER
                 RETURN
@@ -58,7 +58,7 @@ class DefineUuidFunctions extends Migration
             DB::unprepared(<<<SQL
                 -- Turns a uuid string into a binary representation
                 -- Behaves the same as UUID_TO_BIN( ,true)
-                CREATE FUNCTION uuid2bin(_uuid BINARY(36))
+                CREATE FUNCTION reorder_uuid2bin(_uuid BINARY(36))
                 RETURNS BINARY(16)
                 LANGUAGE SQL DETERMINISTIC CONTAINS SQL SQL SECURITY INVOKER
                 RETURN UUID_TO_BIN(_uuid, 1);
@@ -66,12 +66,36 @@ class DefineUuidFunctions extends Migration
 
             DB::unprepared(<<<SQL
                 -- Turns a 16 byte binary into a uuid string
-                CREATE FUNCTION bin2uuid(_bin BINARY(16))
+                CREATE FUNCTION reorder_bin2uuid(_bin BINARY(16))
                 RETURNS BINARY(36)
                 LANGUAGE SQL DETERMINISTIC CONTAINS SQL SQL SECURITY INVOKER
                 RETURN BIN_TO_UUID(_bin, 1);
             SQL);
         }
+        DB::unprepared(<<<SQL
+            -- Turns a uuid string into a binary representation
+            CREATE FUNCTION uuid2bin(_uuid BINARY(36))
+            RETURNS BINARY(16)
+            LANGUAGE SQL DETERMINISTIC CONTAINS SQL SQL SECURITY INVOKER
+            RETURN UNHEX(REPLACE(_uuid, "-",""))
+        SQL);
+
+        DB::unprepared(<<<SQL
+            -- Turns a 16 byte binary into a uuid string
+            CREATE FUNCTION bin2uuid(_uuid BINARY(16))
+            RETURNS BINARY(36)
+            LANGUAGE SQL DETERMINISTIC CONTAINS SQL SQL SECURITY INVOKER
+            BEGIN
+                SET @hex_uuid = HEX(_uuid);
+                RETURN LOWER(CONCAT_WS('-',
+                    SUBSTR(@hex_uuid, 1, 8),
+                    SUBSTR(@hex_uuid, 9, 4),
+                    SUBSTR(@hex_uuid, 13, 4),
+                    SUBSTR(@hex_uuid, 17, 4),
+                    SUBSTR(@hex_uuid, 21)
+                ));
+            END
+        SQL);
     }
 
     /**
@@ -81,6 +105,8 @@ class DefineUuidFunctions extends Migration
      */
     public function down()
     {
+        DB::unprepared("DROP FUNCTION IF EXISTS reorder_uuid2bin");
+        DB::unprepared("DROP FUNCTION IF EXISTS reorder_bin2uuid");
         DB::unprepared("DROP FUNCTION IF EXISTS uuid2bin");
         DB::unprepared("DROP FUNCTION IF EXISTS bin2uuid");
     }
